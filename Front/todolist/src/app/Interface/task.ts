385542ebfea252f796ec6ca5647b292e72ee55e5
gregmavr <gregmavr@users.noreply.github.com>
2019-03-16 13:55:57 +0200

    all

---
+++ Front/todolist/src/app/Interface/task.ts
@@ -0,0 +1,4 @@
+export interface Task{
+  taskid : string;
+  description : string;
+}
---
+++ Front/todolist/src/app/getdbdata/getdbdata.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-getdbdata',
+  templateUrl: './getdbdata.component.html',
+  styleUrls: ['./getdbdata.component.less']
+})
+export class GetdbdataComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
